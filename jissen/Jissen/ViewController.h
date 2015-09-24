7391db1819afaa7c37584c0dece82e7bc2809219
Satoru Sasozaki <satoru@Satorus-MacBook-Pro.local>
2015-09-23 19:15:14 -0700

    init

---
+++ jissen/Jissen/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Jissen
+//
+//  Created by Satoru Sasozaki on 9/22/15.
+//  Copyright © 2015 Satoru Sasozaki. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ jissen/Jissen/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  Jissen
+//
+//  Created by Satoru Sasozaki on 9/22/15.
+//  Copyright © 2015 Satoru Sasozaki. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
