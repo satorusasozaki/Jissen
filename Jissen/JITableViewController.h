e1da3dc52ffb1e78f1e635b0944bcb0a6c37613b
Satoru Sasozaki <sasozakisatoru@gmail.com>
2015-10-04 23:00:46 -0700

    Add scroll method

--- Jissen/JITableViewController.h
+++ Jissen/JITableViewController.h
@@ -4,10 +4,11 @@
 //
 //  Created by Satoru Sasozaki on 9/23/15.
 //  Copyright © 2015 Satoru Sasozaki. All rights reserved.
-//
+//  http://useyourloaf.com/blog/searching-twitter-with-ios.html
+//  http://useyourloaf.com/blog/migrating-to-the-new-twitter-search-api.html
 
 #import <UIKit/UIKit.h>
 
-@interface JITableViewController : UITableViewController <UISearchBarDelegate,UITableViewDelegate>
+@interface JITableViewController : UITableViewController <UISearchBarDelegate,UITableViewDelegate,NSURLConnectionDataDelegate>
 
 @end
--- Jissen/JITableViewController.m
+++ Jissen/JITableViewController.m
@@ -4,16 +4,15 @@
 //
 //  Created by Satoru Sasozaki on 9/23/15.
 //  Copyright © 2015 Satoru Sasozaki. All rights reserved.
-//
-
+//  http://useyourloaf.com/blog/searching-twitter-with-ios.html
+//  http://useyourloaf.com/blog/migrating-to-the-new-twitter-search-api.html
 #import "JITableViewController.h"
 #import <Accounts/Accounts.h>
 #import <Social/Social.h>
 #import "JIDetailViewController.h"
 #import "JITableViewCell.h"
 
-typedef NS_ENUM(NSUInteger, UYLTwitterSearchState)
-{
+typedef NS_ENUM(NSUInteger, UYLTwitterSearchState) {
     UYLTwitterSearchStateLoading = 0,
     UYLTwitterSearchStateNotFound,
     UYLTwitterSearchStateRefused,
@@ -30,7 +29,6 @@ typedef NS_ENUM(NSUInteger, UYLTwitterSearchState)
 @property (nonatomic,strong) NSString *query;
 @property (nonatomic,weak) UISearchBar *searchBar;
 
-@property (nonatomic,strong) UITableView *tableView;
 
 
 @property (nonatomic,weak) JITableViewCell *cell;
@@ -43,8 +41,7 @@ typedef NS_ENUM(NSUInteger, UYLTwitterSearchState)
 @implementation JITableViewController
 
 
-- (ACAccountStore *)accountStore
-{
+- (ACAccountStore *)accountStore {
     if (_accountStore == nil)
     {
         _accountStore = [[ACAccountStore alloc] init];
@@ -52,8 +49,7 @@ typedef NS_ENUM(NSUInteger, UYLTwitterSearchState)
     return _accountStore;
 }
 
-- (NSString *)searchMessageForState:(UYLTwitterSearchState)state
-{
+- (NSString *)searchMessageForState:(UYLTwitterSearchState)state {
     switch (state)
     {
         case UYLTwitterSearchStateLoading:
@@ -80,10 +76,8 @@ typedef NS_ENUM(NSUInteger, UYLTwitterSearchState)
     self.tableView.delegate = self;
 
     UISearchBar *searchBar = [[UISearchBar alloc] initWithFrame:CGRectMake(0, 70, 320, 50)];
-   // UISearchBar *searchBar = [[UISearchBar alloc]init];
     self.searchBar = searchBar;
-    NSMutableArray *results = [NSMutableArray new];
-    self.results = results;
+    self.results = [[NSMutableArray alloc] init];
     self.searchBar.delegate = self;
     [self.tableView.tableHeaderView addSubview:self.searchBar];
     
@@ -96,10 +90,7 @@ typedef NS_ENUM(NSUInteger, UYLTwitterSearchState)
     
     UIRefreshControl *refreshControl = [[UIRefreshControl alloc] init];
     [refreshControl addTarget:self action:@selector(handleRefresh:) forControlEvents:UIControlEventValueChanged];
-//    [self.tableView addSubview:refreshControl];
-  self.refreshControl = refreshControl;
-    
-    
+    self.refreshControl = refreshControl;
 }
 
 - (void)didReceiveMemoryWarning {
@@ -109,23 +100,19 @@ typedef NS_ENUM(NSUInteger, UYLTwitterSearchState)
 
 #pragma mark - <Table view data source>
 
-- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView
-{
+- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView {
     return 1;
 }
 
-- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
-{
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
     NSInteger count = [self.results count];
     return count > 0 ? count : 1;
 
 }
 
-- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
-{
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
     static NSString *ResultCellIdentifier = @"ResultCell";
     static NSString *LoadCellIdentifier = @"LoadingCell";
-    
     
     NSUInteger count = [self.results count];
     if ((count == 0) && (indexPath.row == 0))
@@ -153,11 +140,12 @@ typedef NS_ENUM(NSUInteger, UYLTwitterSearchState)
     
     return cell;
 }
+
 - (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath {
     [tableView deselectRowAtIndexPath:indexPath animated:YES];
     JIDetailViewController *detailViewController = [JIDetailViewController new];
     detailViewController.title = @"Detail";
-    if(self.cell.reuseIdentifier == @"ResultCell") {
+    if([self.cell.reuseIdentifier isEqual:@"ResultCell"]) {
 //  Is it nessesary to be NSDictionary here?
     NSDictionary *tweetDic = (self.results)[indexPath.row];
     NSString *tweetText = tweetDic[@"text"];
@@ -191,8 +179,7 @@ typedef NS_ENUM(NSUInteger, UYLTwitterSearchState)
          {
   
              NSURL *url = [NSURL URLWithString:@"https://api.twitter.com/1.1/search/tweets.json"];
-             NSDictionary *parameters = @{@"count" : RESULTS_PERPAGE,
-                                          @"q" : encodedQuery};
+             NSDictionary *parameters = @{@"count" : RESULTS_PERPAGE, @"q" : encodedQuery};
              
              SLRequest *slRequest = [SLRequest requestForServiceType:SLServiceTypeTwitter
                                                        requestMethod:SLRequestMethodGET
@@ -219,25 +206,29 @@ typedef NS_ENUM(NSUInteger, UYLTwitterSearchState)
 
 #pragma mark - Connection Control
 
-- (void)connection:(NSURLConnection *)connection didReceiveResponse:(NSURLResponse *)response
-{
+- (void)connection:(NSURLConnection *)connection didReceiveResponse:(NSURLResponse *)response {
     self.buffer = [NSMutableData data];
 }
 
 - (void)connection:(NSURLConnection *)connection didReceiveData:(NSData *)data {
-    
     [self.buffer appendData:data];
 }
 
-- (void)connectionDidFinishLoading:(NSURLConnection *)connection
-{
+
+- (void)connectionDidFinishLoading:(NSURLConnection *)connection {
     [UIApplication sharedApplication].networkActivityIndicatorVisible = NO;
     self.connection = nil;
     
     NSError *jsonParsingError = nil;
     NSDictionary *jsonResults = [NSJSONSerialization JSONObjectWithData:self.buffer options:0 error:&jsonParsingError];
     
-    self.results = jsonResults[@"statuses"];
+//    //    self.results = jsonResults[@"statuses"];
+    [self.results addObjectsFromArray:jsonResults[@"statuses"]];
+//    上記のコードが何回も呼ばれてる模様 -> scroll methodをコメントアウトしてなかったから
+    
+// arrayのarrayを作るのはどう？
+    
+    
     if ([self.results count] == 0)
     {
         NSArray *errors = jsonResults[@"errors"];
@@ -257,8 +248,8 @@ typedef NS_ENUM(NSUInteger, UYLTwitterSearchState)
     [self.tableView flashScrollIndicators];
 }
 
-- (void)connection:(NSURLConnection *)connection didFailWithError:(NSError *)error
-{
+
+- (void)connection:(NSURLConnection *)connection didFailWithError:(NSError *)error {
     [UIApplication sharedApplication].networkActivityIndicatorVisible = NO;
     self.connection = nil;
     self.buffer = nil;
@@ -269,8 +260,7 @@ typedef NS_ENUM(NSUInteger, UYLTwitterSearchState)
     [self.tableView reloadData];
 }
 
-- (void)handleError:(NSError *)error
-{
+- (void)handleError:(NSError *)error {
     NSString *errorMessage = [error localizedDescription];
     UIAlertView *alertView = [[UIAlertView alloc] initWithTitle:@"Connection Error"
                                                         message:errorMessage
@@ -280,8 +270,7 @@ typedef NS_ENUM(NSUInteger, UYLTwitterSearchState)
     [alertView show];
 }
 
-- (void)cancelConnection
-{
+- (void)cancelConnection {
     if (self.connection != nil)
     {
         [UIApplication sharedApplication].networkActivityIndicatorVisible = NO;
@@ -314,15 +303,29 @@ typedef NS_ENUM(NSUInteger, UYLTwitterSearchState)
 
 #pragma mark - Refresh Control
 
-- (void)handleRefresh:(id)sender
-{
+- (void)handleRefresh:(id)sender {
     [self loadQuery];
     [self cancelConnection];
 }
 
 
 #pragma mark - Scroll 
+// http://stackoverflow.com/questions/10404116/uitableview-infinite-scrolling/31454471#31454471
+- (void)scrollViewDidScroll:(UIScrollView *)scrollView {
+    CGFloat actualPosition = scrollView.contentOffset.y;
+    // contentSize.height(実際のサイズ)より少し減らしておくことで、そこに到達した時点で（実際のcontentSize.heightに到達する前に）限界を上げてくれる
+    CGFloat contentHeight = scrollView.contentSize.height - 1000;
+    if (actualPosition >= contentHeight) {
+        
+        [self loadQuery];
+        [self.tableView reloadData];
+    }
+}
 
+// 上のメソッドだとスクロールするたびに呼ばれてる模様
+// resultsに入ってる一番下のセルが下に当たったらロードされるようにする
+// さらに、今は同じ検索結果をとってきて追加してるだけ。次の検索結果を追加できるようにする
 
+// API Callのコードを理解する
 
 @end
